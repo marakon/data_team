46eaf35d40c2a640b2fee66d6120f9b2291dff6f
Marakon <mateuszosinski96@gmail.com>
2019-02-21 17:35:03 +0100

    Var refactor

--- main_core.py
+++ main_core.py
@@ -9,19 +9,21 @@ from components.un_tar import Tar
 file_name = input('Enter file name: ')
 folder_name = file_name + "/"
 file = open("values.txt", "w")
+json = JsonOperations()
+count_data = Count()
 
-def present_data(dd, fse):
+def present_data(raw_dd, raw_fse):
     print("Domain data files: ")
-    for data in dd:
+    for data in raw_dd:
         print(data)
     print("FSE data files: ")
-    for data in fse:
+    for data in raw_fse:
         print(data)
     print('')
 
-def read(dd, fse):
-    dd_data = [json.read_json(dd_data) for dd_data in dd]
-    fse_data = [json.read_json(fse_data) for fse_data in fse]
+def read(raw_dd, raw_fse):
+    dd_data = [json.read_json(dd_data) for dd_data in raw_dd]
+    fse_data = [json.read_json(fse_data) for fse_data in raw_fse]
     return dd_data, fse_data
 
 def txt_file_calculation(dd_data, fse_data):
@@ -52,13 +54,12 @@ def pie_plot(dd_data, fse_data):
         plot.save_pie()
 
 Tar(file_name).un_tar_file()
-json = JsonOperations()
-count_data = Count()
-fse, dd = json.categorize_json(folder_name)
-# fse are file system events
-# dd are domain data's
-present_data(dd, fse)
-dd_data, fse_data = read(dd, fse)
+
+raw_fse, raw_dd = json.categorize_json(folder_name)
+# FSE - File System Event
+# DD - Domain Data
+present_data(raw_dd, raw_fse)
+dd_data, fse_data = read(raw_dd, raw_fse)
 
 txt_file_calculation(dd_data, fse_data)
 bar_plot(dd_data, fse_data)
