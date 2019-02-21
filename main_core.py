d790655dd73c35f22d0b789754692df07a212a28
Marakon <mateuszosinski96@gmail.com>
2019-02-21 16:14:09 +0100

    Adding fse_data

--- main_core.py
+++ main_core.py
@@ -23,49 +23,49 @@ def present_data(dd, fse):
         print(data)
     print('')
 
-def read(dd):
+def read(dd, fse):
     dd_data = [json.read_json(dd_data) for dd_data in dd]
-    # fse_data = [json.read_json(fse_data) for fse_data in fse]
-    return dd_data
+    fse_data = [json.read_json(fse_data) for fse_data in fse]
+    return dd_data, fse_data
 
-def txt_file_calculation(dd_data):
+def txt_file_calculation(dd_data, fse_data):
     for data in dd_data:
         mean_dc = "Mean domain count per dc: " + count_data.mean_dc(data)
         sum_storage = "\nStorage used in MB: " + count_data.sum_storage(data)
         file.write(mean_dc)
         file.write(sum_storage)
 
-def bar_plot(dd_data):
+    for data in dd_data:
+        mean_dc = "Mean domain count per dc: " + count_data.mean_dc(data)
+        sum_storage = "\nStorage used in MB: " + count_data.sum_storage(data)
+        file.write(mean_dc)
+        file.write(sum_storage)
+
+def bar_plot(dd_data, fse_data):
     for data in dd_data:
         count_data.data_center(data)
         val, leb, both=count_data.val_leb()
         plot = Plot(val, leb, both)
         plot.save_bar()
 
-def pie_plot(dd_data):
+def pie_plot(dd_data, fse_data):
     for data in dd_data:
         count_data.data_center(data)
         val, leb, both=count_data.val_leb()
         plot = Plot(val, leb, both)
         plot.save_pie()
 
-# Tar(file_name).un_tar_file()
+Tar(file_name).un_tar_file()
 json = JsonOperations()
 count_data = Count()
 fse, dd = json.categorize_json(folder_name)
 # fse are file system events
 # dd are domain data's
 present_data(dd, fse)
-dd_data = read(dd)
+dd_data, fse_data = read(dd, fse)
 
-txt_file_calculation(dd_data)
-bar_plot(dd_data)
-pie_plot(dd_data)
+txt_file_calculation(dd_data, fse_data)
+bar_plot(dd_data, fse_data)
+pie_plot(dd_data, fse_data)
 
 file.close()
-
-
-        # count_data.domain_status(data)
-        # val,leb=count_data.val_leb()
-        # plot=Plot(val,leb)
-        # plot.save_pie()
